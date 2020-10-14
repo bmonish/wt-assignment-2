cbcf4aabfa3ce05e11842fa04df606ca30f99bbb
bmonish <monishmonib@gmail.com>
2020-10-15 00:24:18 +0530

    Header & State Provider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Header from "./components/Header";
 import "./App.css";
+import { auth } from "./firebase";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  return <div className="app"></div>;
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/">
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../css/Header.css";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { auth } from "../firebase";
+
+function Header() {
+  const [{ basket, user }] = useStateValue();
+
+  const handleAuthentication = () => {
+    if (user) {
+      auth.signOut();
+    }
+  };
+
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="header__title">
+          <h1>Web Tech Store</h1>
+        </div>
+      </Link>
+
+      <div className="header__right">
+        <Link to={!user && "/login"}>
+          <div onClick={handleAuthentication} className="header__option">
+            <span>Hello, {user ? `${user.email}` : "Guest"}</span>
+            <br />
+            <span>{user ? "Sign Out" : " Sign In"}</span>
+          </div>
+        </Link>
+        <div className="header__option">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span>{basket?.length}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
